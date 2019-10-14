01b8b003c35387e44339e472b06d46c1c80d9a41
Eddie <eahnivy8@gmail.com>
2019-10-14 16:00:38 +0900

    Initial Commit

---
+++ MiniWebBrowser/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MiniWebBrowser
+//
+//  Created by Eddie Ahn on 2019/10/14.
+//  Copyright © 2019 Eddie Ahn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
